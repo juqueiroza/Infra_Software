438af8dd1b3259472a1474638baab8608b06d11d
Julia Alencar <julia@Brenos-MacBook-Air-2.local>
2022-02-23 22:31:30 -0300

    test

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h> 
+#include <unistd.h>
+#include <stdlib.h>
+
+int main(){
+        pid_t zombie;
+        zombie = fork(); 
+
+        if(zombie == 0) {
+                exit(0);
+        } else {
+                printf("zombie -> pid: %d\n", zombie);
+        }
+
+        while (1) {
+                sleep(10);
+                return(0);
+        }
+}
