26e2022a298e06c4106256271bf08653397ddb71
Julia Queiroz <91906999+juqueiroza@users.noreply.github.com>
2022-02-23 23:35:07 -0300

    Update main.c

--- main.c
+++ main.c
@@ -3,17 +3,18 @@
 #include <stdlib.h>
 
 int main(){
-        pid_t zombie;
-        zombie = fork(); 
+                pid_t zombie;
+        
+                zombie = fork(); 
 
-        if(zombie == 0) {
-                exit(0);
-        } else {
-                printf("zombie -> pid: %d\n", zombie);
-        }
+                if (zombie == 0) {
+                        exit(0);
+                } else {
+                        printf("zombie -> pid: %d\n", zombie);
+                }
 
-        while (1) {
-                sleep(10);
-                return(0);
-        }
+                while (1) {
+                        sleep(10);
+                        return(0);
+                }
 }
